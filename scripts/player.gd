c0e10719e6b33fcb2b8756999d6eb9d9c832e40f
João Pedro Gonçalves <jpedro.fsgs@gmail.com>
2026-07-22 17:16:08 -0300

    Update project configuration and enhance player jump mechanics
    - Increased load steps in level scene for improved performance.
    - Added walls with collision shapes to the level scene.
    - Implemented jump count logic in player script to allow double jumps.

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+const MAX_JUMPS: int = 3
+var jump_count: int = 0
 
 var is_attacking: bool = false
 
@@ -15,10 +17,13 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+	else:
+		jump_count = 0
 
 	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and jump_count < MAX_JUMPS:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 		
 	if Input.is_action_just_pressed("attack") and not is_attacking:
 		attack()
